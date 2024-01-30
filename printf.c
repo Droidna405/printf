37b8ec86d31902adef7939b0930e2f2ee9363917
Droidna405 <joemwangala@gmail.com>
2024-01-31 00:54:59 +0300

    Our printf final commit

--- printf.c
+++ printf.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * _printf - Our custom printf function
  * @format: The format string with specifiers
@@ -12,36 +12,11 @@
 int _printf(const char *format, ...)
 {
 va_list args;
-int count = 0;
+int count;
+
 va_start(args, format);
-while (*format)
-{
-if (*format == '%')
-{
-format++;
-switch (*format)
-{
-case 'c':
-count += write(1, va_arg(args, int), 1);
-break;
-case 's':
-count += write(1, va_arg(args, char *), 1);
-break;
-case '%':
-count += write(1, "%", 1);
-break;
-case 'd':
-case 'i':
-count += printf(1, "%d", va_arg(args, int));
-break;
-}
-}
-else
-{
-count += write(1, format, 1);
-}
-format++;
-}
+count = print_format(format, args);
 va_end(args);
+
 return (count);
 }
