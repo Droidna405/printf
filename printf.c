5c672d8db4bd51af71106df76e9646a8a05141c7
Droidna405 <joemwangala@gmail.com>
2024-01-30 05:58:31 -0500

    main printf function

---
+++ printf.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * _printf - Our custom printf function
+ * @format: The format string with specifiers
+ *
+ * Return: The number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+va_list args;
+int count;
+va_start(args, format);
+count = print_format(format, args);
+va_end(args);
+return (count);
+}
